972250cd505dd75a9cd61a758fdb2c9f59611ccf
rocky-islam <62227570+rocky-islam@users.noreply.github.com>
2022-11-08 19:02:50 +0600

    revile add with section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Typewriter from "typewriter-effect";
 import banner from '../../assets/images/section-side-banner.gif'
 import chicken from '../../assets/images/chicken.jpg'
-import { Fade } from 'react-reveal';
+import { Fade } from "react-reveal";
+import Wobble from "react-reveal/Wobble";
+import Jello from "react-reveal/Jello";
 
 
 const Home = () => {
@@ -45,6 +47,106 @@ const Home = () => {
             </div>
           </div>
         </div>
+        {/* carousel */}
+        <div className="hero md:my-12">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="w-1/2">
+              <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                  <img
+                    alt=""
+                    src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=600"
+                    className="w-full"
+                  />
+                  <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide4" className="btn btn-circle">
+                      ❮
+                    </a>
+                    <a href="#slide2" className="btn btn-circle">
+                      ❯
+                    </a>
+                  </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                  <img
+                    alt=""
+                    src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&w=600"
+                    className="w-full"
+                  />
+                  <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide1" className="btn btn-circle">
+                      ❮
+                    </a>
+                    <a href="#slide3" className="btn btn-circle">
+                      ❯
+                    </a>
+                  </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                  <img
+                    alt=""
+                    src="https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=600"
+                    className="w-full"
+                  />
+                  <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide2" className="btn btn-circle">
+                      ❮
+                    </a>
+                    <a href="#slide4" className="btn btn-circle">
+                      ❯
+                    </a>
+                  </div>
+                </div>
+                <div id="slide4" className="carousel-item relative w-full">
+                  <img
+                    alt=""
+                    src="https://images.pexels.com/photos/918327/pexels-photo-918327.jpeg?auto=compress&cs=tinysrgb&w=600"
+                    className="w-full"
+                  />
+                  <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide3" className="btn btn-circle">
+                      ❮
+                    </a>
+                    <a href="#slide5" className="btn btn-circle">
+                      ❯
+                    </a>
+                  </div>
+                </div>
+                <div id="slide5" className="carousel-item relative w-full">
+                  <img
+                    alt=""
+                    src="https://images.pexels.com/photos/65882/chocolate-dark-coffee-confiserie-65882.jpeg?auto=compress&cs=tinysrgb&w=600"
+                    className="w-full"
+                  />
+                  <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide4" className="btn btn-circle">
+                      ❮
+                    </a>
+                    <a href="#slide1" className="btn btn-circle">
+                      ❯
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-1/2">
+              <Wobble>
+                <h1 className="text-5xl font-bold">Rate Our Food!</h1>
+              </Wobble>
+              <Jello>
+                <p className="py-6">
+                  Rate our food. your feedback is most important for us cause
+                  when you rate our food service then we know about our service
+                  or food quality. and we make more special food for you.
+                </p>
+              </Jello>
+              <button className="btn bg-orange-500 border-none hover:bg-lime-600">
+                Get Started
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* carousel */}
 
         {/* middle section of chicken */}
         <div>
@@ -117,7 +219,7 @@ const Home = () => {
           </div>
         </div>
         {/* test fade */}
-        
+
         {/* test fade */}
       </div>
     );
