6e103ee07c9356c67894da52f9df4ce11cbf70c1
rocky-islam <62227570+rocky-islam@users.noreply.github.com>
2022-11-08 17:35:29 +0600

    revile added with fade

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Typewriter from "typewriter-effect";
 import banner from '../../assets/images/section-side-banner.gif'
 import chicken from '../../assets/images/chicken.jpg'
+import { Fade } from 'react-reveal';
 
 
 const Home = () => {
@@ -44,66 +45,80 @@ const Home = () => {
             </div>
           </div>
         </div>
+
         {/* middle section of chicken */}
         <div>
           <div className="hero  bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
               <div className="w-1/2">
-                <img alt="" src={chicken} className=" rounded-lg shadow-2xl" />
+                <Fade left>
+                  <img
+                    alt=""
+                    src={chicken}
+                    className=" rounded-lg shadow-2xl"
+                  />
+                </Fade>
               </div>
               <div className="w-1/2">
-                <h1 className="text-3xl font-bold">Feel the Sound of Taste</h1>
-                <p className="py-6">
-                  Curabitur ullamcorper ultricies unless. Even CNN. Maecenas
-                  time, the earth needs the sauce of the rhoncus, let it be as
-                  free as ever.
-                </p>
-                <div className="flex justify-between">
-                  <div>
-                    <p>
-                      <img
-                        src="https://cdn.shopify.com/s/files/1/0016/3387/8116/files/img5_100x100.png?v=1613505344"
-                        alt="tandoori"
-                      />
-                    </p>
-                    <p className="text-4xl font-serif">170</p>
-                    <p className="text-xl font-serif">Tandoori</p>
+                <Fade right>
+                  <h1 className="text-3xl font-bold">
+                    Feel the Sound of Taste
+                  </h1>
+                  <p className="py-6">
+                    Curabitur ullamcorper ultricies unless. Even CNN. Maecenas
+                    time, the earth needs the sauce of the rhoncus, let it be as
+                    free as ever.
+                  </p>
+                  <div className="flex justify-between">
+                    <div>
+                      <p>
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0016/3387/8116/files/img5_100x100.png?v=1613505344"
+                          alt="tandoori"
+                        />
+                      </p>
+                      <p className="text-4xl font-serif">170</p>
+                      <p className="text-xl font-serif">Tandoori</p>
+                    </div>
+                    <div>
+                      <p>
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0016/3387/8116/files/chi-2-png_100x100.png?v=1613504101"
+                          alt="Lollipop"
+                        />
+                      </p>
+                      <p className="text-4xl font-serif">524</p>
+                      <p className="text-xl font-serif">Lollipop</p>
+                    </div>
+                    <div>
+                      <p>
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0016/3387/8116/files/img5_100x100--1_100x100.png?v=1613506578"
+                          alt="Breast"
+                        />
+                      </p>
+                      <p className="text-4xl font-serif">175</p>
+                      <p className="text-xl font-serif">Breast</p>
+                    </div>
+                    <div>
+                      <p>
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0016/3387/8116/files/chi-4-png_100x100.png?v=1613504102"
+                          alt="Strips"
+                        />
+                      </p>
+                      <p className="text-4xl font-serif">300</p>
+                      <p className="text-xl font-serif">Strips</p>
+                    </div>
                   </div>
-                  <div>
-                    <p>
-                      <img
-                        src="https://cdn.shopify.com/s/files/1/0016/3387/8116/files/chi-2-png_100x100.png?v=1613504101"
-                        alt="Lollipop"
-                      />
-                    </p>
-                    <p className="text-4xl font-serif">524</p>
-                    <p className="text-xl font-serif">Lollipop</p>
-                  </div>
-                  <div>
-                    <p>
-                      <img
-                        src="https://cdn.shopify.com/s/files/1/0016/3387/8116/files/img5_100x100--1_100x100.png?v=1613506578"
-                        alt="Breast"
-                      />
-                    </p>
-                    <p className="text-4xl font-serif">175</p>
-                    <p className="text-xl font-serif">Breast</p>
-                  </div>
-                  <div>
-                    <p>
-                      <img
-                        src="https://cdn.shopify.com/s/files/1/0016/3387/8116/files/chi-4-png_100x100.png?v=1613504102"
-                        alt="Strips"
-                      />
-                    </p>
-                    <p className="text-4xl font-serif">300</p>
-                    <p className="text-xl font-serif">Strips</p>
-                  </div>
-                </div>
+                </Fade>
               </div>
             </div>
           </div>
         </div>
+        {/* test fade */}
+        
+        {/* test fade */}
       </div>
     );
 };
